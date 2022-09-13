455578c627e7790e5e1fbf8025b2332adbe494a8
Phuong Thanh <phuong74200@gmail.com>
2022-09-13 21:43:49 +0700

    F-Code Management

--- src/store/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-export function createReducer(asyncReducers) {
-    return combineReducers({
-        ...staticReducers,
-        ...asyncReducers,
-    });
-}

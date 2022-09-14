6762c82cac32f35c53a8a8c2b374f360068be0bd
Nghĩa Nef <trongnghia143@outlook.com.vn>
2022-09-14 11:20:26 +0700

    ADD - Create local storage ultils and persisted state to store user or token

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,8 @@
 const publicRuntimeConfig = {
     NODE_ENV: process.env.NODE_ENV || 'production',
     API_URL: process.env.REACT_APP_API_URL,
+    LOCAL_STORAGE_TOKEN: process.env.REACT_APP_TOKEN_NAME,
 };
 
-export const { NODE_ENV, API_URL } = publicRuntimeConfig;
+export const { NODE_ENV, API_URL, LOCAL_STORAGE_TOKEN } = publicRuntimeConfig;
 export default publicRuntimeConfig;

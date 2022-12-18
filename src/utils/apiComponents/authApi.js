38144a4d0db4510840df0fc45aeffece59b27770
List Kelvin <phamm5687@gmail.com>
2022-12-19 00:13:34 +0700

    UPDATE handle error

--- src/utils/apiComponents/authApi.js
+++ src/utils/apiComponents/authApi.js
@@ -1,6 +1,8 @@
 import { get } from '../ApiCaller';
 import localStorageUtils from '../localStorageUtils';
 
+import { toastError } from '@/components/ToastNotification';
+
 const token = localStorageUtils.getItem('token');
 const authApi = {
     // subject api
@@ -8,30 +10,29 @@ const authApi = {
         const endpoint = '/login/member';
         return await get(endpoint, {}, {})
             .then((res) => {
-                if (res.data.code !== 200) console.log(res.data.message);
+                if (res.data.code !== 200) toastError(res.data.message);
                 return res;
             })
-            .catch((err) => console.log(err.message));
+            .catch((err) => toastError(err.message));
     },
     //need role manager or admin to call
     getAllMembers: async () => {
-        console.log(token);
         const endpoint = '/member/all';
         return await get(endpoint, {}, { authorization: token })
             .then((res) => {
-                if (res.data.code !== 200) console.log(res.data.message);
+                if (res.data.code !== 200) toastError(res.data.message);
                 return res;
             })
-            .catch((err) => console.log(err.message));
+            .catch((err) => toastError(err.message));
     },
     getUser: async () => {
         const endpoint = '/member/own';
         return await get(endpoint, {}, { authorization: token })
             .then((res) => {
-                if (res.data.code !== 200) console.log(res.data.message);
+                if (res.data.code !== 200) toastError(res.data.message);
                 return res;
             })
-            .catch((err) => console.log(err.message));
+            .catch((err) => toastError(err.message));
     },
 };
 export default authApi;

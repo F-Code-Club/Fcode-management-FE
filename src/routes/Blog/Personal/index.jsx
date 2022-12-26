1288a5594f0daf78f0c291ad7ed920eaec9b3828
HaiDang <dangtranhai628@gmail.com>
2022-12-26 12:43:51 +0700

    FIX - Hover button with !important, error when delete blog, centered 'Create blog' button

--- src/routes/Blog/Personal/index.jsx
+++ src/routes/Blog/Personal/index.jsx
@@ -17,8 +17,8 @@ import usePersistedState from '@/utils/usePersistedState';
 const { Search } = Input;
 
 const PersonalBlog = () => {
-    const token = localStorageUtils.getToken();
-    const tokens = usePersistedState('token', token)[0];
+    const tokenInLocal = localStorageUtils.getToken();
+    const token = usePersistedState('token', tokenInLocal)[0];
 
     const [blogs, setBlogs] = useState({
         all: useSelector(selectCurrentBlog).author,
@@ -37,24 +37,20 @@ const PersonalBlog = () => {
 
             dispatch(changeBlog({}));
             setBlogs({ ...blogs, loading: true, isDelete: false });
-            await dispatch(getAllBlogs(tokens))
-                .then((res) => {
-                    setBlogs({
-                        ...blogs,
-                        loading: false,
-                        isDelete: false,
-                        all: res.payload.author?.length ? res.payload.author : [],
-                        search: res.payload.author?.length ? res.payload.author : [],
-                    });
-                })
-                .finally(() => {
-                    setBlogs({ ...blogs, loading: false, isDelete: false });
+            await dispatch(getAllBlogs(token)).then((res) => {
+                setBlogs({
+                    ...blogs,
+                    loading: false,
+                    isDelete: false,
+                    all: res.payload.author?.length ? res.payload.author : [],
+                    search: res.payload.author?.length ? res.payload.author : [],
                 });
+            });
         })();
     }, [blogs.isDelete]); // If a blog is deleted, it will re-render to get all blogs
 
     const deleteBlog = async (id) => {
-        const res = await articleApi.deleteArticle(id);
+        const res = await articleApi.deleteArticle(id, token);
         if (res.data.code === 200) {
             toastSuccess('Xoá bài viết thành công!');
             setBlogs({ ...blogs, isDelete: true });

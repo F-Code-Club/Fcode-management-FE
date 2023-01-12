11bd7dc9af26f155eee7797b3805b5310af07597
Ducbao <baondforwork@gmail.com>
2023-01-13 01:26:13 +0700

    valid for the studentId

--- src/routes/EditAccount/components/uploadAva/index.js
+++ src/routes/EditAccount/components/uploadAva/index.js
@@ -32,7 +32,15 @@ export const UploadImage = ({ loading, setLoading }) => {
             centered
         >
             <UploadTwoUrl>
-                {newUrl && <Image src={newUrl.trim()} alt="ava" height={300} />}
+                <Image
+                    src={
+                        newUrl.trim() ||
+                        'https://s3-alpha-sig.figma.com/img/45c7/b834/5cbf35abd1e4216bda5c0e9e6bee66f4?Expires=1674432000&Signature=p2r50hSIU0V3hwLMIGXbtfIa~oVXxPzukpP-1CFgXRD0TA~hZB~Zlw0Q63Bm3UJXlMaYAEYxud-t16n-bvMyYta~jE30BWxJhzGP4xrDRuHSloUp7AAwhBJFQKX1AwwR8I5M4UutmTvhZAOVv41I6lOV6gX1m9EpiarvO7oDi7fELisSVcIOr2FNUxFmqokuUM-LWZKuvG~sY08nET7BcSYTv70zFVr1sKPaKRuzMAaepB2AqM2B-f1tG5jbRJVmdFB2yqD5V8nwMUAvYLKISL7osvUmZgyKGjGJawi-UoZ0PYmCeFfUnrdY5tPWEzvE0w~vzQP89LwjjKdXJ255VQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+                    }
+                    alt="ava"
+                    height={300}
+                />
+
                 <br />
                 <Input
                     value={newUrl}
